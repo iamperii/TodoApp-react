6678d1fb1cf4be489ba4fb313971590d49cabc46
Memmedova Peri <perimemmedova20052022@gmail>
2024-12-09 20:24:07 +0400

    ToDoCreate component done.

--- src/Components/ToDoCreate/ToDoCreate.jsx
+++ src/Components/ToDoCreate/ToDoCreate.jsx
@@ -1,7 +1,18 @@
-import ToDoList from '../ToDoList/ToDoList';
+import { useState } from 'react';
 import './ToDoCreate.css';
 
-const ToDoCreate = () => {
+const ToDoCreate = ({ onCreateTodo }) => {
+	// console.log(onCreateTodo);
+	const [newTodo, setNewTodo] = useState('');
+	const createTodo = () => {
+		if (!newTodo) return;
+
+		const request = {
+			id: Math.floor(Math.random() * 99999999),
+			content: newTodo,
+		};
+		onCreateTodo(request);
+	};
 	return (
 		<>
 			<div className="to-do">
@@ -10,10 +21,15 @@ const ToDoCreate = () => {
 					name="to-do"
 					id="to-do"
 					placeholder="Enter your todos..."
+					value={newTodo}
+					onChange={(e) => {
+						setNewTodo(e.target.value);
+					}}
 				/>
-				<button className="create-button">Create Todo</button>
+				<button className="create-button" onClick={createTodo}>
+					Create Todo
+				</button>
 			</div>
-			<ToDoList />
 		</>
 	);
 };
