e37c154166f1b254d988a28421ad5eb4a71ea86b
Memmedova Peri <perimemmedova20052022@gmail>
2024-12-09 20:22:21 +0400

    createTodo func.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react';
 import './App.css';
 import ToDoCreate from './Components/ToDoCreate/ToDoCreate';
+import ToDoList from './Components/ToDoList/ToDoList';
 
 function App() {
+	const [todos, setTodos] = useState([]);
+
+	const createTodo = (newTodo) => {
+		setTodos([...todos, newTodo]);
+	};
+	console.log(todos);
+
 	return (
 		<>
-			<ToDoCreate />
+			<ToDoCreate onCreateTodo={createTodo} />
+			<ToDoList />
 		</>
 	);
 }
